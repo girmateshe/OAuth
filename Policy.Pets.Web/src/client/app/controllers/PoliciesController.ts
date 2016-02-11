edc2999111db83fc2e7093871af2a1eeb59ce61e
girmateshe <girmateshe@gmail.com>
2016-02-11 14:33:14 -0800

    Added JSON-> HTML template

--- Policy.Pets.Web/src/client/app/controllers/PoliciesController.ts
+++ Policy.Pets.Web/src/client/app/controllers/PoliciesController.ts
@@ -10,6 +10,7 @@ module App.Controllers {
         policy: rs.IPetPolicy;
         countries: Array<rs.ICountry>;
         vm: PoliciesController;
+        table: any;
     }
 
     export class PoliciesController {
@@ -17,6 +18,57 @@ module App.Controllers {
         public init(): void {
             this.$scope.countries = this.country.query();
             console.log(this.$scope.countries);
+
+            this.$scope.table = {
+                name: "Registrations",
+                summary: [
+                    {
+                        name: "Total Registrations by TLD",
+                        value: [
+                            { name: ".COM registrations", value: 23 },
+                            { name: ".NGO registrations", value: 23 },
+                            { name: ".ONG registrations", value: 23 }
+                        ]
+                    },
+                    {
+                        name: "Total New Registrations:",
+                        value: 49
+                    },
+                    {
+                        name: "Average Registration Period:",
+                        value: 1.22
+                    }
+                ],
+                fields: [
+                            { name: "Order ID", title: "A nicer human readable label or title for the field" },
+                            { name: "Domain", title: "A nicer human readable label or title for the field" },
+                            { name: "Creation Date", title: "A nicer human readable label or title for the field" },
+                            { name: "Exp Date", title: "A nicer human readable label or title for the field" },
+                            { name: "Years", title: "A nicer human readable label or title for the field" },
+                            { name: "Paid Amount", title: "A nicer human readable label or title for the field" },
+                            { name: "Tax Amount", title: "A nicer human readable label or title for the field" },
+                            { name: "Billed to Customer", title: "A nicer human readable label or title for the field" }
+                ],
+                rows: [
+                    [161939331, '3oniedcrypto3oiterol.ngo', '1/4/2016', '1/5/2017', 1, '$0.00', '$0.00', '$0.00'],
+                    [161939331, '3oniedcrypto3oiterol.ngo', '1/4/2016', '1/5/2017', 1, '$0.00', '$0.00', '$0.00'],
+                    [161939331, '3oniedcrypto3oiterol.ngo', '1/4/2016', '1/5/2017', 1, null, '$0.00', '$0.00'],
+                    [161939331, '3oniedcrypto3oiterol.ngo', '1/4/2016', '1/5/2017', 1, '$0.00', null, '$0.00'],
+                    [161939331, '3oniedcrypto3oiterol.ngo', '1/4/2016', '1/5/2017', 1, '$0.00', '$0.00', '$0.00'],
+                    [161939331, '3oniedcrypto3oiterol.ngo', '1/4/2016', '1/5/2017', 1, '$0.00', '$0.00', '$0.00']
+                ],
+                pagination: {
+                    currentPage: 1,
+                    totalPages: 7,
+                    pageSize: 50
+                },
+                sort: {
+                    by: 'domain',
+                    order: 'asc'
+                }
+            };
+
+            console.log(this.$scope.table);
         }
 
         public static $inject = [
